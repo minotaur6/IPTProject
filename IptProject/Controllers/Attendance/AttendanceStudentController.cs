d07eefdace4bbb95413d569cc20070920167e2db
Jusani <yousufjusani24@gmail.com>
2020-05-28 15:02:58 +0500

    Model Attendance Created

--- IptProject/Controllers/Attendance/AttendanceStudentController.cs
+++ IptProject/Controllers/Attendance/AttendanceStudentController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +12,27 @@ namespace IptProject.Controllers.Attendance
         // GET: AttendanceStudent
         public ActionResult ViewAttendance()
         {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/");
+                //HTTP GET
+                //var responseTask = client.GetAsync("attendance/get");
+                //responseTask.Wait();
+
+                //var result = responseTask.Result;
+                //if (result.IsSuccessStatusCode)
+                //{
+
+                    //var readTask = result.Content.ReadAsAsync<FoodItem[]>();
+                    //readTask.Wait();
+
+                    //var fooditems = readTask.Result;
+
+                    
+                //}
+            }
+
             return View();
         }
     }
