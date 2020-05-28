2607d649fd433b12436704bb1dcf5ec470e6b539
minotaur6 <officiallymina@gmail.com>
2020-05-28 17:24:08 +0500

    Attendance Model commit

--- IptProject/Models/Attendance/Attendance.cs
+++ IptProject/Models/Attendance/Attendance.cs
@@ -7,8 +7,11 @@ namespace IptProject.Models.Attendance
 {
     public class Attendance
     {
-
-
-
+        public int AttendanceID { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public string AttendanceStatus { get; set; }
+        public int ClassDuration { get; set; }
+        public int EnrollmentID { get; set; }
+        
     }
 }
