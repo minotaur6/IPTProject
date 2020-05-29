f9f6cd2fe5a264db258f5a19ed220711aa20f1ad
ridatariq19 <57638431+ridatariq19@users.noreply.github.com>
2020-05-29 23:20:00 +0500

    view attendance controller and view

--- IptProject/Controllers/Attendance/AttendanceStudentController.cs
+++ IptProject/Controllers/Attendance/AttendanceStudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IptProject.Models.Attendance;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -12,36 +13,34 @@ namespace IptProject.Controllers.Attendance
         // GET: AttendanceStudent
         public ActionResult ViewAttendance()
         {
-
+            List<StudentCourseAttendance> checkAttendance = new List<StudentCourseAttendance>();
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:44380/api/");
                 //HTTP GET
+                int studentId = 10;
+                var responseTask = client.GetAsync("AttendanceStudent/ViewAttendance/" + studentId);
+                responseTask.Wait();
 
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<StudentCourseAttendance[]>();
+                    readTask.Wait();
 
-
-
-
-
-
-
-                //var responseTask = client.GetAsync("attendance/get");
-                //responseTask.Wait();
-
-                //var result = responseTask.Result;
-                //if (result.IsSuccessStatusCode)
-                //{
-
-                    //var readTask = result.Content.ReadAsAsync<FoodItem[]>();
-                    //readTask.Wait();
+                    var studentAttendance = readTask.Result;
+                    foreach (var stud in studentAttendance)
+                    {
+                        checkAttendance.Add(stud);
+                    }
 
                     //var fooditems = readTask.Result;
 
-                    
-                //}
-            }
+                    //}
+                }
 
-            return View();
+                return View(checkAttendance);
+            }
         }
     }
 }
